1725e49ccca2828d412f1b13a37ce66c39f3dd7a
Maxime Pelte <Maxime Pelte>
2017-02-03 17:08:30 +0100

    Adding resources files & splash screen

---
+++ app/src/main/java/com/maxim/ezstudio/SplashScreen.java
@@ -0,0 +1,25 @@
+package com.maxim.ezstudio;
+
+import android.content.Intent;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SplashScreen extends AppCompatActivity {
+    private final int SPLASH_SCREEN_TIMEOUT = 4000;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent mainIntent = new Intent(SplashScreen.this, MainActivity.class);
+                SplashScreen.this.startActivity(mainIntent);
+                SplashScreen.this.finish();
+            }
+        }, SPLASH_SCREEN_TIMEOUT);
+    }
+}

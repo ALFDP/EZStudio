4e0aab25701ff27b9bbc4676b1bbbaa51b8c2db6
Sorion <maxime.pelte@gmail.com>
2017-02-04 17:38:30 +0100

    Adding Database connexion helper

---
+++ app/src/main/java/com/maxim/ezstudio/core/BaseDAO.java
@@ -0,0 +1,35 @@
+package com.maxim.ezstudio.core;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+
+/**
+ * Created by maxim on 04/02/2017.
+ */
+
+public abstract class BaseDAO {
+    // Base Version
+    protected final static int VERSION = 1;
+    // Base name
+    protected final static String NAME = "database.db";
+
+    protected SQLiteDatabase db = null;
+    protected DataBaseHelper dbHelper = null;
+
+    public BaseDAO(Context context) {
+        this.dbHelper = new DataBaseHelper(context, NAME, null, VERSION);
+    }
+
+    public SQLiteDatabase open() {
+        db = dbHelper.getWritableDatabase();
+        return db;
+    }
+
+    public void close() {
+        db.close();
+    }
+
+    public SQLiteDatabase getDb() {
+        return db;
+    }
+}

b456cbf110d4fc13e7d9e01c7321ffe23169c073
Sorion <maxime.pelte@gmail.com>
2017-02-04 17:23:53 +0100

    Adding mdd classes

---
+++ app/src/main/java/com/maxim/ezstudio/core/Album.java
@@ -0,0 +1,33 @@
+package com.maxim.ezstudio.core;
+
+/**
+ * Created by maxim on 04/02/2017.
+ */
+
+public class Album extends Project {
+    private String compositor;
+    private String release;
+
+
+    public Album(long id, String name, String date, String compositor, String release) {
+        super(id, name, date);
+        this.compositor = compositor;
+        this.release = release;
+    }
+
+    public String getCompositor() {
+        return compositor;
+    }
+
+    public void setCompositor(String compositor) {
+        this.compositor = compositor;
+    }
+
+    public String getRelease() {
+        return release;
+    }
+
+    public void setRelease(String release) {
+        this.release = release;
+    }
+}
---
+++ app/src/main/java/com/maxim/ezstudio/core/Track.java
@@ -0,0 +1,39 @@
+package com.maxim.ezstudio.core;
+
+import java.lang.reflect.Array;
+
+/**
+ * Created by maxim on 04/02/2017.
+ */
+
+public class Track extends Project {
+    private String compositor;
+    private long[] links;
+
+    public Track(String compositor, long[] links) {
+        this.compositor = compositor;
+        this.links = links;
+    }
+
+    public Track(long id, String name, String date, String compositor, long[] links) {
+        super(id, name, date);
+        this.compositor = compositor;
+        this.links = links;
+    }
+
+    public String getCompositor() {
+        return compositor;
+    }
+
+    public void setCompositor(String compositor) {
+        this.compositor = compositor;
+    }
+
+    public long[] getLinks() {
+        return links;
+    }
+
+    public void setLinks(long[] links) {
+        this.links = links;
+    }
+}

7a213e53f38aa83284c4ba2417f2517f21f7345a
Sorion <maxime.pelte@gmail.com>
2017-02-12 00:29:09 +0100

    Updating the new project layout, updating strings resources

--- app/src/main/java/com/maxim/ezstudio/NewProjectActivity.java
+++ app/src/main/java/com/maxim/ezstudio/NewProjectActivity.java
@@ -12,15 +12,23 @@ public class NewProjectActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_new_project);
-        setTitle(R.string.new_project);
+        setTitle(R.string.new_string);
 
         Bundle bundle = getIntent().getExtras();
 
-        ProjectType type;
+        ProjectType type = null;
 
         if(bundle != null) {
            type = (ProjectType) bundle.getSerializable("projectType");
             Toast.makeText(this, type.toString(), Toast.LENGTH_SHORT).show();
+        }
+
+        if(ProjectType.ALBUM == type) {
+            setTitle(getString(R.string.new_album_project));
+        } else if(ProjectType.TRACK == type) {
+            setTitle(getString(R.string.new_track_project));
+        } else {
+            setTitle(getString(R.string.new_project));
         }
     }
 }

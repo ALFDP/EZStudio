551f537dfcb9877bb13abe261b9e873e1f65edc3
Sorion <maxime.pelte@gmail.com>
2017-02-12 12:43:46 +0100

    Changing theme app, adding cancel interaction

--- app/src/main/java/com/alfdp/ezstudio/NewProjectActivity.java
+++ app/src/main/java/com/alfdp/ezstudio/NewProjectActivity.java
@@ -2,11 +2,15 @@ package com.alfdp.ezstudio;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.Toast;
 
 import com.alfdp.ezstudio.core.ProjectType;
 
 public class NewProjectActivity extends AppCompatActivity {
+
+    private ProjectType projectType = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -14,18 +18,32 @@ public class NewProjectActivity extends AppCompatActivity {
         setContentView(R.layout.activity_new_project);
         setTitle(R.string.new_string);
 
+        this.setProjectType();
+        this.initButtons();
+    }
+
+    private void initButtons() {
+        final Button cancel = (Button) findViewById(R.id.btn_cancelCreation);
+
+        cancel.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
+    }
+
+    private void setProjectType() {
+
         Bundle bundle = getIntent().getExtras();
-
-        ProjectType type = null;
-
         if(bundle != null) {
-           type = (ProjectType) bundle.getSerializable("projectType");
-            Toast.makeText(this, type.toString(), Toast.LENGTH_SHORT).show();
+            projectType = (ProjectType) bundle.getSerializable("projectType");
+            Toast.makeText(this, projectType.toString(), Toast.LENGTH_SHORT).show();
         }
 
-        if(ProjectType.ALBUM == type) {
+        if(ProjectType.ALBUM == projectType) {
             setTitle(getString(R.string.new_album_project));
-        } else if(ProjectType.TRACK == type) {
+        } else if(ProjectType.TRACK == projectType) {
             setTitle(getString(R.string.new_track_project));
         } else {
             setTitle(getString(R.string.new_project));

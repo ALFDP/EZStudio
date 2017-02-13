20c8a3ce354e09b357ad74215dccf76fcb31f867
Sorion <maxime.pelte@gmail.com>
2017-02-13 16:50:11 +0100

    Adding method to check if app run for the firt time

--- app/src/main/java/com/alfdp/ezstudio/SplashScreen.java
+++ app/src/main/java/com/alfdp/ezstudio/SplashScreen.java
@@ -1,10 +1,13 @@
 package com.alfdp.ezstudio;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Handler;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
 
 public class SplashScreen extends AppCompatActivity {
     private final int SPLASH_SCREEN_TIMEOUT = 4000;
@@ -17,6 +20,8 @@ public class SplashScreen extends AppCompatActivity {
         ActionBar actionBar = getSupportActionBar();
         actionBar.hide();
 
+        checkFirstRun();
+
         new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
@@ -25,5 +30,54 @@ public class SplashScreen extends AppCompatActivity {
                 SplashScreen.this.finish();
             }
         }, SPLASH_SCREEN_TIMEOUT);
+    }
+
+    private void checkFirstRun() {
+
+        final String PREFS_NAME = "MyPrefsFile";
+        final String PREF_VERSION_CODE_KEY = "version_code";
+        final int NOT_EXIST = -1;
+
+
+        // Get current version code
+        int currentVersionCode = 0;
+        try {
+            currentVersionCode = getPackageManager().getPackageInfo(getPackageName(), 0).versionCode;
+        } catch (android.content.pm.PackageManager.NameNotFoundException e) {
+            // handle exception
+            e.printStackTrace();
+            return;
+        }
+
+        // Get saved version code
+        SharedPreferences prefs = getSharedPreferences(PREFS_NAME, MODE_PRIVATE);
+        int savedVersionCode = prefs.getInt(PREF_VERSION_CODE_KEY, NOT_EXIST);
+
+        // Check for first run or upgrade
+
+        if (currentVersionCode == savedVersionCode) {
+
+            // This is just a normal run
+            Log.e("First RUN", "FALSE");
+            Toast.makeText(this, "LATEST UPDATE", Toast.LENGTH_SHORT);
+            return;
+
+        } else if (savedVersionCode == NOT_EXIST) {
+
+            // TODO This is a new install (or the user cleared the shared preferences)
+            Log.e("First RUN", "TRUE");
+            Toast.makeText(this, "FIRST CONFIG", Toast.LENGTH_SHORT);
+
+        } else if (currentVersionCode > savedVersionCode) {
+
+            // TODO This is an upgrade
+            Log.e("First RUN", "UPGRADE");
+            Toast.makeText(this, "UPGRADE CONFIG", Toast.LENGTH_SHORT);
+
+        }
+
+        // Update the shared preferences with the current version code
+        prefs.edit().putInt(PREF_VERSION_CODE_KEY, currentVersionCode).commit();
+
     }
 }
